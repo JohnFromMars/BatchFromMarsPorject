a2d8da810860180955e4c56b76c7733527224371
JohnFormMars <Yj@Yj-PC>
2016-11-27 01:47:04 +0800

    #2016/11/27
    log formatter

--- BatchFromMarsProject/src/com/batchfrommars/file/BatchLogFormatter.java
+++ BatchFromMarsProject/src/com/batchfrommars/file/BatchLogFormatter.java
@@ -6,13 +6,14 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class BatchLogFormatter extends Formatter {
-	private final SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	private final SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private final String OOUTE = " : ";
 
 	@Override
 	public String format(LogRecord record) {
-		String message = form.format(new Date()) + OOUTE + "[" + record.getSourceMethodName() + "] ["
-				+ record.getLevel() + "] " + record.getMessage() + "\r\n";
+		String message = form.format(new Date()) + OOUTE
+				+  "[" + record.getSourceMethodName() + "] " +String.format("%1$-8s", record.getLevel()) + "- "
+				+ record.getMessage() + "\r\n";
 
 		return message;
 	}
