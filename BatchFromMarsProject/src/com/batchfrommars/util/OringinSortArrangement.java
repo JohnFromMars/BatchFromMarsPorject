72fe5969333eef52a28907baa00a8a2c813a8264
user <user@user-PC>
2017-11-04 16:47:53 +0800

    sort origin

--- BatchFromMarsProject/src/com/batchfrommars/util/OringinSortArrangement.java
+++ BatchFromMarsProject/src/com/batchfrommars/util/OringinSortArrangement.java
@@ -7,12 +7,16 @@ import java.util.logging.Logger;
 import com.batchfrommars.component.ComponentII;
 import com.batchfrommars.component.SortComponent;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
+
 public class OringinSortArrangement implements SortUtil {
 
 	@Override
 	public void sortArrangement(List<ComponentII> components, Logger log, String sortText) {
 
-		log.finest("sortArrangement start");
+		log.finest("sortArrangement start, sort text=" + sortText);
+		ArrayList<SortObject> result = analyseSortText(sortText, log);
+		log.finest("result=" + result);
 
 		// set sort component
 		SortComponent sortComponent = new SortComponent() {
@@ -24,12 +28,12 @@ public class OringinSortArrangement implements SortUtil {
 
 			@Override
 			protected ArrayList<Integer> getOrders() {
-				return getOrderFromSortText(sortText);
+				return getOrderFromSortText(result, log);
 			}
 
 			@Override
 			protected ArrayList<Object> getKeys(String data) {
-				return getKeyFromSortText(sortText, data);
+				return getKeyFromSortText(result, data, log);
 			}
 		};
 
@@ -41,25 +45,131 @@ public class OringinSortArrangement implements SortUtil {
 
 	}
 
-	private ArrayList<Integer> getOrderFromSortText(String sortText) {
-		String[] strings = sortText.split(",");
+	private ArrayList<Integer> getOrderFromSortText(ArrayList<SortObject> result, Logger log) {
 
-		for (int i = 0; i < strings.length - 1; i++) {
+		ArrayList<Integer> orders = new ArrayList<>();
 
-			if (i % 3 == 2) {
-				if (strings[i].matches("\\d+")) {
-
-				}
-			}
+		for (SortObject sortObject : result) {
+			orders.add(sortObject.getOrder());
+			log.finest("add order=" + sortObject.getOrder());
 		}
-		
-		return null;
+		log.finest("orders = " + orders);
+
+		return orders;
 
 	}
 
-	private ArrayList<Object> getKeyFromSortText(String sortText, String data) {
+	private ArrayList<Object> getKeyFromSortText(ArrayList<SortObject> result, String data, Logger log) {
+		ArrayList<Object> key = new ArrayList<>();
 
-		return null;
+		for (SortObject sortObject : result) {
+			key.add(data.substring(sortObject.getStart() - 1, sortObject.getEnd()));
+			log.finest("add key=" + (sortObject.getStart() - 1) + ", " + sortObject.getEnd());
+		}
+
+		return key;
+	}
+
+	private ArrayList<SortObject> analyseSortText(String sortText, Logger log) {
+		int leftPointer = 0;
+		ArrayList<SortObject> sortObjects = new ArrayList<>();
+		String[] strings = sortText.split(",");
+
+		while (leftPointer < strings.length) {
+			log.finest("leftPointer=" + leftPointer + ", strings.length=" + (strings.length));
+
+			SortObject sortObject = new SortObject();
+
+			for (int i = leftPointer; i < strings.length; i++) {
+				log.finest("i=" + i);
+
+				if (strings[i].matches("\\d+")) {
+
+					if (sortObject.getStart() == null && sortObject.getEnd() == null) {
+						log.finest("sortObject.getStart() == null && sortObject.getEnd() == null");
+						log.finest("sortObject = " + sortObject.toString());
+						log.finest("strings[i]=" + strings[i]);
+						sortObject.setStart(Integer.parseInt(strings[i]));
+						log.finest("after set=" + sortObject);
+
+					} else if (sortObject.getStart() != null && sortObject.getEnd() == null
+							&& Integer.parseInt(strings[i]) >= sortObject.getStart()) {
+						log.finest(
+								"sortObject.getStart() != null  sortObject.getEnd() == nullInteger.parseInt(strings[i]) >= sortObject.getStart()");
+						log.finest("sortObject = " + sortObject.toString());
+						log.finest("strings[i]=" + strings[i]);
+						sortObject.setEnd(Integer.parseInt(strings[i]));
+						log.finest("after set=" + sortObject);
+
+					} else if (sortObject.getStart() != null && sortObject.getEnd() != null) {
+						log.finest("sortObject.getStart() != null && sortObject.getEnd() != null");
+						log.finest("sortObject = " + sortObject.toString());
+						log.finest("strings[i]=" + strings[i]);
+						sortObject.setOrder(1);
+						sortObjects.add(sortObject);
+						leftPointer = i;
+
+						log.finest("after set=" + sortObject);
+						log.finest("after set leftPointer = " + leftPointer);
+						log.finest("sortObjects=" + sortObjects);
+						break;
+
+					} else {
+						log.finest("SyntaxException");
+						throw new SyntaxException(sortText + " please check the syntax of the stament.");
+					}
+
+				} else if (strings[i].equals("a") || strings[i].equals("A") || strings[i].equals("D")
+						|| strings[i].equals("d")) {
+
+					log.finest("in condition, strings[i]=" + strings[i]);
+
+					if (sortObject.getStart() == null || sortObject.getEnd() == null) {
+						log.finest("SyntaxException");
+						throw new SyntaxException(sortText + " please check the syntax of the stament.");
+
+					} else if (sortObject.getStart() != null && sortObject.getEnd() != null) {
+						log.finest("sortObject = " + sortObject.toString());
+						log.finest("strings[i]=" + strings[i]);
+
+						if (strings[i].equals("A") || strings[i].equals("a")) {
+							sortObject.setOrder(1);
+						} else {
+							sortObject.setOrder(-1);
+						}
+						log.finest("**sortObject = " + sortObject.toString());
+						sortObjects.add(sortObject);
+						leftPointer = i + 1;
+						break;
+
+					} else {
+						log.finest("SyntaxException");
+						throw new SyntaxException(sortText + " please check the syntax of the stament.");
+					}
+
+				} else {
+					log.finest("SyntaxException");
+					throw new SyntaxException(sortText + " please check the syntax of the stament.");
+				}
+
+				leftPointer = strings.length;
+			}
+
+			if (sortObject.getOrder() == null && sortObject.getStart() != null && sortObject.getEnd() != null) {
+				sortObject.setOrder(1);
+				sortObjects.add(sortObject);
+
+			}
+
+			if (sortObject.getOrder() == null && sortObject.getStart() != null && sortObject.getEnd() == null) {
+				log.finest("SyntaxException");
+				throw new SyntaxException(sortText + " please check the syntax of the stament.");
+			}
+
+		}
+
+		log.finest("Sort object = " + sortObjects.toString());
+		return sortObjects;
 	}
 
 }
--- BatchFromMarsProject/test/com/batchfrommars/test/controller/BatchControllerTest.java
+++ BatchFromMarsProject/test/com/batchfrommars/test/controller/BatchControllerTest.java
@@ -16,9 +16,9 @@ public class BatchControllerTest {
 		};
 
 		batchController.addLogger("BatchControllerTest", "D:/BatchFromMars", LogeLevel.FINEST)
-				       .addInput("D://BatchFromMars/TestSortComponent/StepSort_D20170129.txt", "BIG5")
+				       .addInput("D:/BatchFromMars/SortData/sort1.txt", "UTF8")
 				       .addOutput("D:/BatchFromMars/TTEESST.txt", "BIG5", false)
-				       .sort("1,3,A")
+				       .sort("4,6,1,3,d")
 				       .execute();
 	}
 
