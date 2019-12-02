2f513e61dcb13a4b179e3a1863d38b2755092431
JohnFromMars <rock13810@gmail.com>
2019-12-02 10:13:17 +0800

    File information comment

--- BatchFromMarsProject/src/com/batchfrommars/file/FileInformation.java
+++ BatchFromMarsProject/src/com/batchfrommars/file/FileInformation.java
@@ -1,22 +1,53 @@
 package com.batchfrommars.file;
 
 /**
+ * File information interface
  * 
  * @author JohnFromMars
  * @date 2016-09-17
  */
 public interface FileInformation {
 
+	/**
+	 * read a line of string form the file
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	String readFile() throws Exception;
 
-	void writeFile(String data)throws Exception;
+	/**
+	 * write a line of string into a file
+	 * 
+	 * @param data
+	 * @throws Exception
+	 */
+	void writeFile(String data) throws Exception;
 
+	/**
+	 * check if the file is empty
+	 * 
+	 * @return
+	 */
 	boolean isEmpty();
 
+	/*
+	 * Close the related resources of reading and writing the file
+	 */
 	void closeFile() throws Exception;
-	
+
+	/**
+	 * Delete the file
+	 * 
+	 * @throws Exception
+	 */
 	void deleteFile() throws Exception;
-	
+
+	/**
+	 * Show information for debugging
+	 * 
+	 * @return
+	 */
 	String toString();
 
 }
